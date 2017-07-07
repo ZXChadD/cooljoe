51b1634b2b26d2c0669b5acf51bad6afd1ebb263
lerhhl <lerhhl@gmail.com>
2017-07-07 10:13:55 +0800

    Enhance model_controller_generator

--- lib/generators/basic_setup_generator.rb
+++ lib/generators/basic_setup_generator.rb
@@ -3,6 +3,7 @@ class BasicSetupGenerator < Rails::Generators::Base
     # rake "db:create"
     # generate "devise:install"
     # generate "devise User"
+    # generate "migration AddOmniauthToUsers provider:string uid:string"
     # generate "devise:views"
     # generate "rspec:install"
     # generate "mailer mailername"
--- lib/generators/model_controller_generator.rb
+++ lib/generators/model_controller_generator.rb
@@ -1,8 +1,28 @@
 class ModelControllerGenerator < Rails::Generators::Base
   def create_initializer_file
-    # generate "model", "topic title:string description:text user:references"
-    # generate "controller", "topics index show"
-    # route "resources :topics only: [:index]"
+    # generate "model", "provider firstname:string lastname:string tel_num:string email:string license_num:string description:text experience:string avatar:string"
+    # generate "controller", "providers index show new create edit update destroy --skip-routes"
+    # route "resources :providers"
+
+    # generate "model", "like user:references provider:references"
+    # generate "controller", "likes create destroy --skip-routes"
+    # route "resources :likes"
+
+    # generate "model", "provider_attach image:string title:string provider:references"
+
+    # generate "model", "job type:string description:text fixture:string num_fixture:int housing:string job_address:text user:references provider:references status:int date:string time:string"
+    # generate "controller", "jobs index show new create edit update destroy --skip-routes"
+    # route "resources :jobs"
+
+    # generate "model", "job_attach image:string title:string job:references"
+
+    # generate "model", "schedule date:string time:string provider:references"
+
+    # generate "model", "invoice provider:references job:references invoice_ref:string price:decimal precision:10 scale:2 "
+    # generate "controller", "invoices index show new create edit update destroy --skip-routes"
+    # route "resources :invoice"
+
+    # route "resources :??, only: [:index, :show]"
     # rake "db:migrate"
   end
 end
