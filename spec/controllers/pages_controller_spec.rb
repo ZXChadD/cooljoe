77eee4eeb188842ad6450530598d0e985628ed5f
lerhhl <lerhhl@gmail.com>
2017-07-08 11:05:25 +0800

    Remove some codes in rspec files

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET #about" do
     it "returns http success" do
       get :about
