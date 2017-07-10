e9cc5c7a4ffb946dfa5040392c1ffa8c1d01b2a7
hadi91 <ahmad_nurhadi@hotmail.com>
2017-07-11 00:37:43 +0800

    Add image upload for both joblisting and provider avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname address tel_num])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname lastname tel_num user_address license_num description experience avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname address tel_num experience description license_num avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname lastname address tel_num experience description license_num avatar])
   end
 
 end
