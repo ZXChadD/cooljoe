ff5e2a9653eb0b62c40c1397096bc5b8f8b150bf
ZXChadd <zhexianchadd@gmail.com>
2017-07-10 23:04:50 +0800

    Add CSS to provider index

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,13 +4,13 @@ class Schedule < ApplicationRecord
 
   validates :date, :time, presence: true
 
+  scope :weekends, -> {where(date: 'weekends')}
+  scope :weekdays, -> {where(date: 'weekdays')}
+  scope :alldays, -> {where(date: 'all')}
+
   before_save do
     self.date.gsub!(/[\[\]\"]/, "") if attribute_present?("date")
     self.time.gsub!(/[\[\]\"]/, "") if attribute_present?("time")
   end
-
-  scope :weekends, -> {where(date: 'weekends')}
-  scope :weekdays, -> {where(date: 'weekdays')}
-  scope :alldays, -> {where(date: 'all')}
 
 end
