fe16784d09f05330ac333b76b198d4c7b00fd977
ZXChadd <zhexianchadd@gmail.com>
2017-07-13 13:16:46 +0800

    Added comments for providers and edited css for edit user and provider profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712133805) do
+ActiveRecord::Schema.define(version: 20170713034531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "provider_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider_id"], name: "index_comments_on_provider_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "conversations", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -151,6 +161,8 @@ ActiveRecord::Schema.define(version: 20170712133805) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "providers"
+  add_foreign_key "comments", "users"
   add_foreign_key "conversations", "providers"
   add_foreign_key "conversations", "users"
   add_foreign_key "invoices", "joblistings"
